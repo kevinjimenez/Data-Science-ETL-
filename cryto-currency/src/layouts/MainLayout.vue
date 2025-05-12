18fcdd9d21a7b808e9695373b4d6a21b6ca881d1
Kevin Jimenez <orlando.kj24@gmail.com>
2025-05-12 12:56:28 -0500

    feat: 🚧 front con vue

---
+++ cryto-currency/src/layouts/MainLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="flex flex-col w-full min-h-screen">
+    <router-view />
+  </main>
+</template>
+<script setup lang="ts"></script>
--- cryto-currency/src/main.ts
+++ cryto-currency/src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
+import './assets/styles.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
